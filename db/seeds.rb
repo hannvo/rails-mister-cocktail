6825659ba7fb680c559ea8026dd36bd6059ced2e
Hannah Voget <hannah@voget.de>
2021-02-18 17:03:00 +0100

    added card style to index and seeded with ingredients api

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,10 @@
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
+require 'json'
+require 'open-uri'
 
-Cocktail.create(name: "Havana Club")
-Cocktail.create(name: "Dry Martini")
+serialized = URI.open('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list').read
+JSON.parse(serialized)['drinks'].each do |drink|
+  Ingredient.create(name: drink['strIngredient1'])
+end
+
+Cocktail.create(name: "Havana Club", image_url: 'http://alkoholecapone.pl/wp-content/uploads/2016/09/Havana-Club-3.jpg')
+Cocktail.create(name: "Dry Martini", image_url: 'https://www.annatomasmoreno.com/wp-content/uploads/2015/01/DRY-MARTINI-2.jpg')
