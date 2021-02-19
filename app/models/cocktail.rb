882c5d103217aa080a552c1edba37f17cc290375
Hannah Voget <hannah@voget.de>
2021-02-19 15:28:27 +0100

    implemented imageupload and storing in cloudinary

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,5 +1,8 @@
 class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
+  has_one_attached :photo
   validates :name, presence: true, uniqueness: true
+
+  DEFAULT_IMAGE_URL = "https://ginie.de/wp-content/uploads/Martini.jpg"
 end
