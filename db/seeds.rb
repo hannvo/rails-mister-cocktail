deb683ae84b3f00e22ddfd23d201b13071f2047e
Hannah Voget <hannah@voget.de>
2021-02-19 12:55:24 +0100

    Add dotenv - Protect my secret data in .env file, plus some styling and removing of image_url from cocktails

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@ JSON.parse(serialized)['drinks'].each do |drink|
   Ingredient.create(name: drink['strIngredient1'])
 end
 
-Cocktail.create(name: "Havana Club", image_url: 'http://alkoholecapone.pl/wp-content/uploads/2016/09/Havana-Club-3.jpg')
-Cocktail.create(name: "Dry Martini", image_url: 'https://www.annatomasmoreno.com/wp-content/uploads/2015/01/DRY-MARTINI-2.jpg')
+Cocktail.create(name: "Havana Club")
+Cocktail.create(name: "Dry Martini")
